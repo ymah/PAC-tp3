e2789056e7f1550ee5be9004033f4e46d24d1627
Mahieddine Yaker <mahieddine.yaker@gmail.com>
2015-03-23 14:05:47 +0100

    ajout de la fonction qui retrouve le next de rand

--- rand/partie3.py
+++ rand/partie3.py
@@ -1,6 +1,6 @@
 from client import *
 from random import randint
-import math
+from math import *
 from ctypes import c_uint32
 
 
@@ -19,3 +19,24 @@ challenge='challenge/'+NOM
 tabIV=server.query(url=URL_PARTIE+challenge)
 
 print(tabIV)
+
+def rand(n):
+    next = n * 1103515245 + 12345
+    return floor((next/65536) % 32768)
+
+
+def invRand(n):
+    res = 0
+    i = 0
+    while res != n:
+        res = i % 32768
+        i+=1
+    res *= 65536
+    return ((res - 12345)/ 1103515245)
+
+next=invRand(tabIV['IV'][0])
+
+
+
+print(next)
+print(rand(next))
